c2eddb413178ad60aec173823edfc234b09fd1f8
Phil Pearson <Phil.Pearson@vizcom.com.au>
2015-12-24 16:19:15 +0800

    Initial Commit

---
+++ HideAndSeek/Outside.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideAndSeek
+{
+	class Outside : Location
+	{
+		private bool hot;
+		public Outside(bool hot, string name)
+			:base (name) {
+			this.hot = hot;
+		}
+
+		public bool Hot {
+			get { return hot; }
+		}
+
+		public override string Description {
+			get {
+				return base.Description;
+			}
+		}
+	}
+}
---
+++ HideAndSeek/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideAndSeek
+{
+	class Room : Location
+	{
+		private string decoration;
+		public Room(string decoration, string name)
+			:base (name) {
+			this.decoration = decoration;
+		}
+
+		public string Decoration {
+			get { return decoration; }
+		}
+
+		public override string Description {
+			get {
+				return base.Description;
+			}
+		}
+	}
+}
