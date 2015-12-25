1dba307a5df0737d32b19483b4cc6ba6575a7f62
Phil Pearson <Phil.Pearson@vizcom.com.au>
2015-12-25 10:53:43 +0800

    Added IHidingPlace and OutsideWithHidingPlace. Need to check my code against book to confirm its correct

---
+++ HideAndSeek/OutsideWithHidingPlace.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HideAndSeek
+{
+	class OutsideWithHidingPlace: Outside, IHidingPlace
+	{
+		private string hidingPlace;
+		public string HidingPlace {
+			get { return hidingPlace; }
+		}
+
+		public OutsideWithHidingPlace(string name, bool hot, string hidingPlace)
+			: base (name,hot) {
+			this.hidingPlace = hidingPlace;
+		}
+	}
+}
